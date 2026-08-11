1742ff57e3e47e92986c6fd7a4ddcc27095737fb
Sihao Lu <ben@wispr.ai>
2026-08-11 12:37:54 -0700

    Initial Commit

---
+++ BotSpeaker/BotSpeakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotSpeakerApp.swift
+//  BotSpeaker
+//
+//  Created by Sihao Lu on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct BotSpeakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
